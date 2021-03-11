325efff3b578bfa4d656d863da3401af49f0abff
Alexis COUVREUR <acouvreur@supralog.com>
2021-03-11 14:20:27 +0100

    refactor: remove conveyal workflow

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 module.exports = {
   prepare: require('./lib/prepare'),
-  publish: require('./lib/publish'),
   verifyConditions: require('./lib/verify-conditions'),
   verifyRelease: require('./lib/verify-release')
 }
